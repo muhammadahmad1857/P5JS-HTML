12b3d813e3c7749c944f5b05d390a6799376a70f
= <ahmedjawad1857@gmail.com>
2025-02-27 00:49:39 +0500

    Adjust canvas size and center page styling

--- project_02_trigonometry/sketch.js
+++ project_02_trigonometry/sketch.js
@@ -1,55 +1,3 @@
-// let x, y;
-// let angle = 0;
-// let r = 150;
-// let shiftingAngle = [];
-// let numAxis = 10;
-// let x2 = [];
-// let y2 = [];
-// function setup() {
-//   createCanvas(400, 400);
-//   angleMode(DEGREES);
-//   for (let i = 0; i < numAxis; i++) {
-//     shiftingAngle[i] = (i * 90) / numAxis;
-//   }
-// }
-
-// function draw() {
-//   background(79, 148, 167);
-//   stroke(255, 100);
-//   // angle = map(mouseX, 0, width, 0, 368); // If you wanna do it with mouse movement
-//   x = r * cos(angle);
-//   y = r * sin(angle);
-
-//   translate(width / 2, height / 2);
-//   noFill();
-//   ellipse(0, 0, r * 2, r * 2);
-//   fill(0);
-//   ellipse(x, y, 20, 20);
-
-//   // fill(255);
-//   // ellipse(x, 0, 20, 20);
-//   // ellipse(0, y, 20, 20);
-
-//   // line(-r, 0, r, 0);
-//   // line(0, -r, 0, r);
-//   for (let i = 0; i < numAxis; i++) {
-//     x2[i] = r * cos(angle + shiftingAngle[i]);
-//     y2[i] = r * sin(angle + shiftingAngle[i]);
-//     push();
-//     rotate(-shiftingAngle[i]);
-//     // stroke(0, 0, 255);
-//     line(-r, 0, r, 0);
-//     // stroke(0, 255, 0);
-//     line(0, -r, 0, r);
-
-//     fill(255);
-//     ellipse(x2[i], 0, 20, 20);
-//     ellipse(0, y2[i], 20, 20);
-//     pop();
-//   }
-//   angle += 1;
-// }
-
 let x, y;
 let angle = 0;
 let r = 150;
@@ -59,7 +7,7 @@ let x2 = [];
 let y2 = [];
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(300, 300);
   angleMode(DEGREES);
   for (let i = 0; i < numAxis; i++) {
     shiftingAngle[i] = (i * 90) / numAxis;
