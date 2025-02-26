5ed5e83143b581c9d9d1e2a537acc79690261000
= <ahmedjawad1857@gmail.com>
2025-02-26 23:36:15 +0500

    Project 1 added

---
+++ project_01_rotating_blocks/sketch.js
@@ -0,0 +1,30 @@
+let angle = 0;
+let distMouse = 20;
+let size = 10;
+let cols = 4;
+let rows = 4;
+let offset = 4;
+let blocks = [];
+function setup() {
+  createCanvas(400, 400);
+  rectMode(CENTER);
+  angleMode(DEGREES);
+  cols = width / size;
+  rows = height / size;
+  for (let i = 0; i < cols; i++) {
+    blocks[i] = [];
+    for (let j = 0; j < rows; j++) {
+      blocks[i][j] = new Block(size / 2 + i * size, size / 2 + j * size);
+    }
+  }
+}
+
+function draw() {
+  background(0);
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      blocks[i][j].move();
+      blocks[i][j].display();
+    }
+  }
+}
